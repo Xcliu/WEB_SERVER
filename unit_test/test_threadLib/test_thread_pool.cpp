d0aec8c45642aa356e3130434c3a09f0730731cd
root <1107296046@qq.com>
2021-04-07 20:47:01 +0800

    add dynamic resize methond in thrad pool

--- unit_test/test_threadLib/test_thread_pool.cpp
+++ unit_test/test_threadLib/test_thread_pool.cpp
@@ -5,8 +5,15 @@
 #include <thread>
 #include <sstream>
 #include <mutex>
+#include <chrono>         // std::chrono::seconds
+#include <random>
 
-static std::mutex mtx;
+int generate_random_int(const int beg,const int end){
+    std::random_device sd; 
+    std::minstd_rand linear_ran(sd());
+    std::uniform_int_distribution<int> dis(beg,end);
+    return dis(linear_ran);
+}
 
 std::string get_tid(){
     std::ostringstream ss;
@@ -14,16 +21,39 @@ std::string get_tid(){
     return ss.str();
 }
 
-void task_func(std::string str){
-    std::lock_guard<std::mutex> lg(mtx);
-    std::cout << get_tid() << " -> " << str << std::endl; 
+int task_func(std::string str){
+    std::string temp = get_tid() + " -> " + str ;
+    std::cout << temp << std::endl; 
+    std::this_thread::sleep_for (std::chrono::microseconds(generate_random_int(30,40)));
+    return 1;
 }
 
 int main(int argc,char *argv[]){
     {
-        SONNIE::Thread_pool<void, std::string> tp(3,100);
-        tp.run();
-        for(int i = 0; i<20 ; i++){
+        SONNIE::Thread_pool<int, std::string> tp(3,100,0);
+        std::cout << "thread pool size = 3 " << std::endl;
+        for(int i = 0; i< 6 ; i++){
+            tp.add_task(task_func,std::to_string(i));
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "thread pool size = 1 " << std::endl;
+        tp.decrease_pool_size(2);
+        for(int i = 6; i< 10 ; i++){
+            tp.add_task(task_func,std::to_string(i));
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "thread pool size = 2 " << std::endl;
+        tp.increase_pool_size(1);
+        for(int i = 10; i< 20 ; i++){
+            tp.add_task(task_func,std::to_string(i));
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "thread pool size = 3 " << std::endl;
+        tp.increase_pool_size(1);
+        for(int i = 20; i< 26 ; i++){
             tp.add_task(task_func,std::to_string(i));
         }
     }
--- utils/ThreadLib/thread_pool.hpp
+++ utils/ThreadLib/thread_pool.hpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <utility>
 #include <iostream>
+#include <future>
+#include <chrono>
 
 namespace SONNIE{
     
@@ -14,38 +16,48 @@ template< typename R, typename... Args>
 class Thread_pool{
     public:
         using task_t = std::function<R(void)>;
+        // the return type of task cant not be "void"
+        using task_r_t = typename task_t::result_type;
 
-        explicit Thread_pool(size_t sz, int64_t queue_tiemout)
-                :thread_pool_size_(sz),task_queue_(queue_tiemout){}
+        explicit Thread_pool(size_t sz, int64_t queue_tiemout, int64_t task_timeout_ms)
+                :task_queue_(queue_tiemout),task_timeout_ms_(task_timeout_ms){
+            increase_pool_size(sz);   
+        }
         
         virtual ~Thread_pool(){
             exit_tag_ = true;
-            for(size_t i = 0 ; i< thread_vec_.size(); ++i){
+            for(size_t i = 0 ; i < pool_tail_ptr_; ++i){
                 thread_vec_[i].join();
-            }
-            std::cout << "destruct func" << std::endl;
-        }
-    
-        void run(){
-            for(size_t i = 0;i < thread_pool_size_; ++i){
-                thread_vec_.emplace_back([this](){
-                    while(true){
-                        // 后续需要在这里给每个task设置一个超时时间
-                        task_t task_obj; 
-                        if(this->task_queue_.wait_and_pop(task_obj)){
-                            task_obj();
-                        // task_queue is empty and exit_tag_ is false;
-                        }else if(exit_tag_){
-                            break;
-                        }
-                    }
-                });
             }
         }
 
-        // NOTE(lxc): how to save the variable parameters for the next calling
-        // 有两种解决方法 std::bind 生成一个新的void(void)的可调用对象
-        // 或者使用std::tuple 来存参数包 
+        void increase_pool_size(int delta_num){
+            for(int i = 0; i < delta_num; ++i){
+                int index = pool_tail_ptr_;
+                if( index <= static_cast<int>(thread_vec_.size())-1){
+                    thread_vec_[index] = std::thread(task_lambad_,index);
+                    task_status_[index].clear();
+                    ++pool_tail_ptr_; 
+                }else{
+                    thread_vec_.emplace_back(task_lambad_,index);
+                    task_status_.emplace_back();
+                    ++pool_tail_ptr_;
+                }
+            }
+        }
+
+        void decrease_pool_size(int delta_num){
+            delta_num = delta_num >= pool_tail_ptr_? pool_tail_ptr_/2:delta_num;
+            int count = 0;
+            while(count < delta_num ){
+                int index = pool_tail_ptr_-1;
+                task_status_[index].can_be_deleted_ = true;
+                thread_vec_[index].join();
+                --pool_tail_ptr_;
+                ++count;
+            }
+        }
+
         void add_task(const std::function<R(Args...)> f, Args&&... args){
             if(!exit_tag_){
                 task_queue_.push(std::bind(f,std::forward<Args...>(args...)));
@@ -53,10 +65,49 @@ class Thread_pool{
         }
 
     private:
-        const size_t thread_pool_size_;
+        std::function<void(int)> task_lambad_{
+            [this](int index){
+                while(true){
+                    // TODO(lxc): 后续需要给每个task设置一个超时时间
+                    // 1. 开一个新线程，在新线程中执行task，通过线程超时控制来实现，但是这这样做线程池就没意义了
+                    // 2. ？
+                    task_t task_obj;
+                    if(this->task_queue_.wait_and_pop(task_obj)){
+                        task_status_[index].latest_return_val_ = task_obj();
+                    }else if(exit_tag_){
+                        break;
+                    }
+                    // NOTE(lxc) exit_tag_ 和 can_be_deleted_ 的区别在于
+                    // 完全销毁线程池之前(exit_tag_)需要把task_queue中的task执行完
+                    // 但是线程池缩容时(can_be_deleted_)则不需要等待清空task_queue
+                    if(task_status_[index].can_be_deleted_){
+                        break;
+                    }
+                }
+            }
+        };
+        struct task_status_info{
+           task_r_t latest_return_val_; 
+           // NOTE(lxc):up to now, the return vaule of task has not be used.
+           bool  can_be_deleted_{false};
+           
+           task_status_info() = default;
+
+           void clear(){
+               can_be_deleted_=false;
+           }
+        };
+
+        int pool_tail_ptr_{0};
+        
+        // the item having same index in thread_vec_ and task_status_
+        // realize a mapping from thread instance to task_status_info;
         std::vector<std::thread> thread_vec_;
+        std::vector<task_status_info> task_status_;
+
         SONNIE::Thread_safe_queue<task_t> task_queue_;
         bool exit_tag_{false};
+        const int64_t task_timeout_ms_;
 };
 
 } // namspace SONNIE
